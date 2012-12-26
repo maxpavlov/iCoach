42bb26d14ffb7cda71c76c0eae973b25c41a7759
Max Pavlov <max@radacode.com>
2012-12-26 13:47:54 +0200

    On to UI

---
+++ iCoach/Scripts/global.js
@@ -0,0 +1,29 @@
+﻿var iCoach = window.iCoach || {};
+
+iCoach.Lang = (function ($) {
+    "use strict";
+
+    var init = function () {
+        iCoach.Lang.SwitchLanguage = function (lang) {
+            $.cookie('language', lang, { expires: 365, path: '/' });
+            window.location.reload();
+        };
+
+        $("#setRus").click(function () {
+            iCoach.SwitchLanguage('ru');
+        });
+        $("#setEng").click(function () {
+            iCoach.SwitchLanguage('en');
+        });
+    };
+
+    return { init: init };
+}($));
+
+iCoach.GlobalLogic = (function ($) {
+    "use strict";
+
+    var ready = $(function () {
+        iCoach.Lang.init();
+    });
+}($));
